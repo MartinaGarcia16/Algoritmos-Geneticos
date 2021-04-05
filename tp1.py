b1ed1be905fcc0fbfa5c58b6df78aed1cf3aa85c
sofiaerrecarte <sofiaerrecarte@gmail.com>
2021-04-05 11:59:08 -0300

    calcular fitness + acomodar para mostrar los datos + cambiar el envio de parametros para solo los que necesitaba y usar como variables globales

--- tp1.py
+++ tp1.py
@@ -1,4 +1,107 @@
-import random
+from random import randint
+n=0
+cantidad_pi=10
+cantidad_genes=30
+cromosomas_binario=[]
+cromosomas_decimal=[]
+lista_funcion_obj=[]
+fitness=[]
+
+
+def crearPoblacionBinario():
+ for x in range(cantidad_pi):
+    cromosomas_binario.append([])
+    for j in range(cantidad_genes):
+        n=randint(0,1)
+        cromosomas_binario[x].append(n) 
+
+def crearLista(a,lista):
+    for x in range(a):
+        lista.append(None)
+
+def convertirDecimal():
+    for x in range(cantidad_pi):
+        suma=0
+        for j in range(cantidad_genes):
+            if cromosomas_binario[x][j]==1:
+                suma+=(2**(cantidad_genes-(j+1)))
+        cromosomas_decimal[x]=suma
+
+def funcionObjetivo(a):
+  return  (a/((2**30)-1))**2
+
+def calculaSPM():
+    suma=0
+    max=lista_funcion_obj[0]
+    for x in range(cantidad_pi):
+        suma+=lista_funcion_obj[x]
+        if (lista_funcion_obj[x]>= max):
+         max=lista_funcion_obj[x]
+    lista_funcion_obj[cantidad_pi]=suma
+    lista_funcion_obj[cantidad_pi+1]=max
+    lista_funcion_obj[cantidad_pi+2]= (suma/cantidad_pi)
+
+#calculo el fitness de cada cromosoma 
+def calcular_fitness(suma,maxi,mini):
+    for i in range(10):
+        x= (lista_funcion_obj[i]/suma)
+        fitness[i] = x
+
+#main
+#creando y cargando la matriz de cromosomas (poblacion inicial)
+crearPoblacionBinario()
+#mostrando las matriz binaria
+print("POBLACION INICIAL BINARIA")
+for x in range(10):
+    for j in range(30):
+        print(cromosomas_binario[x][j],end=" ")
+    print()
+#creando lista decimal
+crearLista(cantidad_pi, cromosomas_decimal)
+#creo lista de fitness
+crearLista(cantidad_pi,fitness)
+#convirtiendo a decimal
+convertirDecimal()
+#mostrando decimal 
+print("\nPOBLACION INICIAL EL DECIMALES")  
+for x in range(10):
+    print(cromosomas_decimal[x])
+#crear valores funcion objetivo (le agrego 3 lugares mas para guardar max, suma y promedio)
+crearLista((cantidad_pi+3),lista_funcion_obj)
+#valores funcion objetivo
+for x in range(cantidad_pi):
+    lista_funcion_obj[x]= funcionObjetivo(cromosomas_decimal[x])
+
+#calcular suma promedio maximo
+calculaSPM()
+suma= lista_funcion_obj[10]
+maxi= lista_funcion_obj[11]
+mini = lista_funcion_obj[12]
+#calcular fitness
+calcular_fitness(suma,maxi,mini)
+#mostrar funcion objetivo
+print('\nFUNCION OBJETIVO')
+for i in range (10):
+    print(lista_funcion_obj[i])
+'''for x in range(13):
+    if x==10:
+        print("suma")
+    if x==11:
+        print("maximo")
+    if x==12:
+        print("promedio")
+    print(lista_funcion_obj[x])'''
+print('\nSUMA:', end=" ") 
+print(suma)
+print('MAXIMO:', end=" ") 
+print(maxi)
+print('MINIMO:', end=" ") 
+print(mini)
+print('\nFITNESS')
+for i in range (len(fitness)):
+    print(fitness[i])
+
+"""import random
 import collections
 import statistics
 import numpy
@@ -51,7 +154,7 @@ for i in range(0, ciclos):
 print(decimales)
 print(fitness)
 
-"""sacar maximo
+sacar maximo
 maximo=-1
     for arg in args:
         if arg > maximo:
